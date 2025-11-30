dcf33e1470b9dd03407f38eb27412e01108612dc
Harsh Vaddoriya <harshvaddoriya0319@gmail.com>
2025-11-30 21:42:57 +0530

    add table data

--- mf-users/pages/tabledata.tsx
+++ mf-users/pages/tabledata.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import Layout from "../components/Layout";
 import AgUserTable from '@components/AgUserTable';
 import { users } from 'data/users';
 
 export default function TableData() {
   return (
-    <Layout>
-        <div className='p-4'>
-            <h1 className='font-semibold'>Table Data with Crud Operations:</h1>
+   
+        <div className='p-2'>
+            <h1 className='font-semibold text-2xl'>Users Data : </h1>
              <AgUserTable users={users} />
         </div>
-    </Layout>
+    
     
   )
 }
--- shell-app/remotes/analysisComponents.js
+++ shell-app/remotes/analysisComponents.js
@@ -14,3 +14,4 @@ export const TableDataPage = dynamic(
 //     () => import("analysis/SummaryCards"),
 //     { ssr: false }
 // );
+
